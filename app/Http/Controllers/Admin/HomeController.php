c8ae215de1ff166ea1eca6174a317272f86ad3cc
marcosborg <m.borges.mail@gmail.com>
2023-09-06 14:00:06 +0100

    Update

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -8,23 +8,7 @@ class HomeController
 {
     public function index()
     {
-        $settings1 = [
-            'chart_title' => 'Ranking de faturação semanal por motoristas',
-            'chart_type' => 'bar',
-            'report_type' => 'group_by_string',
-            'model' => 'App\Models\TvdeActivity',
-            'group_by_field' => 'driver_code',
-            'aggregate_function' => 'sum',
-            'aggregate_field' => 'earnings_one',
-            'filter_field' => 'created_at',
-            'column_class' => 'col-md-12',
-            'entries_number' => '5',
-            'translation_key' => 'tvdeActivity',
-        ];
-
-        $chart1 = new LaravelChart($settings1);
-
-        return view('home', compact('chart1'));
+        return view('home');
     }
 
     public function selectCompany($company_id)

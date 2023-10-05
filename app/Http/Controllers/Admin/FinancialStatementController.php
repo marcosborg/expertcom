790e4150247a3852365339ce2aa9f22577bbe089
marcosborg <m.borges.mail@gmail.com>
2023-10-05 15:33:00 +0100

    Update

--- app/Http/Controllers/Admin/FinancialStatementController.php
+++ app/Http/Controllers/Admin/FinancialStatementController.php
@@ -30,19 +30,33 @@ class FinancialStatementController extends Controller
 
         $company_id = session()->get('company_id') ?? $company_id = session()->get('company_id');
         $tvde_year_id = session()->get('tvde_year_id') ? session()->get('tvde_year_id') : $tvde_year_id = TvdeYear::orderBy('name')->first()->id;
-        $tvde_month_id = session()->get('tvde_month_id') ? session()->get('tvde_month_id') : $tvde_month_id = TvdeMonth::orderBy('number', 'desc')
-            ->whereHas('weeks', function ($week) use ($company_id) {
-                $week->whereHas('tvdeActivities', function ($tvdeActivity) use ($company_id) {
-                    $tvdeActivity->where('company_id', $company_id);
-                });
-            })
-            ->where('year_id', $tvde_year_id)
-            ->first()->id;
+        if (session()->has('tvde_month_id')) {
+            $tvde_month_id = session()->get('tvde_month_id');
+        } else {
+            $tvde_month = TvdeMonth::orderBy('number', 'desc')
+                ->whereHas('weeks', function ($week) use ($company_id) {
+                    $week->whereHas('tvdeActivities', function ($tvdeActivity) use ($company_id) {
+                        $tvdeActivity->where('company_id', $company_id);
+                    });
+                })
+                ->where('year_id', $tvde_year_id)
+                ->first();
+            if ($tvde_month) {
+                $tvde_month_id = $tvde_month->id;
+            } else {
+                $tvde_month_id = 0;
+            }
+        }
         if (session()->has('tvde_week_id')) {
             $tvde_week_id = session()->get('tvde_week_id');
         } else {
-            $tvde_week_id = TvdeWeek::orderBy('number', 'desc')->where('tvde_month_id', $tvde_month_id)->first()->id;
-            session()->put('tvde_week_id', $tvde_week_id);
+            $tvde_week = TvdeWeek::orderBy('number', 'desc')->where('tvde_month_id', $tvde_month_id)->first();
+            if ($tvde_week) {
+                $tvde_week_id = $tvde_week->id;
+                session()->put('tvde_week_id', $tvde_week->id);
+            } else {
+                $tvde_week_id = 0;
+            }
         }
         $driver_id = session()->get('driver_id') ? session()->get('driver_id') : $driver_id = 0;
 

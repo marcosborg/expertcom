cf0a5c1df6009222dab892edf92a59c195c661e6
marcosborg <m.borges.mail@gmail.com>
2023-10-17 15:51:15 +0100

    Update

--- app/Http/Controllers/Admin/WeeklyExpenseInputController.php
+++ app/Http/Controllers/Admin/WeeklyExpenseInputController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\CompanyExpense;
+use App\Models\TvdeWeek;
+use App\Models\WeeklyExpense;
 use Gate;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,7 +16,40 @@ class WeeklyExpenseInputController extends Controller
     {
         abort_if(Gate::denies('weekly_expense_input_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        return view('admin.weeklyExpenseInputs.index');
+        if (session()->has('company_id') && session()->get('company_id') != 0) {
+            $company_id = session()->get('company_id');
+        } else {
+            $company_id = 0;
+        }
+
+        if (session()->has('tvde_week_id')) {
+            $tvde_week_id = session()->get('tvde_week_id');
+        } else {
+            $tvde_week_id = 0;
+        }
+
+        $tvde_weeks = TvdeWeek::all();
+
+        $company_expenses = CompanyExpense::where([
+            'company_id' => $company_id
+        ])
+            ->get();
+
+        $weekly_expense = WeeklyExpense::where([
+            'company_id' => $company_id,
+            'tvde_week_id' => $tvde_week_id
+        ])->first();
+
+        return view(
+            'admin.weeklyExpenseInputs.index',
+            compact(
+                'company_id',
+                'company_expenses',
+                'tvde_week_id',
+                'tvde_weeks',
+                'weekly_expense'
+            )
+        );
     }
 
 }
--- resources/views/admin/weeklyExpenseInputs/index.blade.php
+++ resources/views/admin/weeklyExpenseInputs/index.blade.php
@@ -9,9 +9,56 @@
                     {{ trans('cruds.weeklyExpenseInput.title') }}
                 </div>
                 <div class="panel-body">
-                    <p>
-                        Text coming soon...
-                    </p>
+                    @if ($company_id != 0)
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>Semana</label>
+                                <select name="tvde_week_id" class="form-control select2">
+                                    <option selected disabled>Selecionar semana</option>
+                                    @foreach ($tvde_weeks as $tvde_week)
+                                    <option {{ $tvde_week->id == $tvde_week_id ? 'selected' : '' }} value="{{
+                                        $tvde_week->id
+                                        }}">{{ $tvde_week->start_date }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    @foreach ($company_expenses as $company_expense)
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>Despesa</label>
+                                <input type="text" disabled value="{{ $company_expense->name }}" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>Quantidade</label>
+                                <input type="number" min="0" class="form-control" name="qty">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <label>Semanal</label>
+                            <input type="text" class="form-control" disabled
+                                value="{{ $company_expense->weekly_value }} €">
+                        </div>
+                    </div>
+                    @endforeach
+                    <div class="form-group">
+                        <button class="btn btn-success">Gravar despesas</button>
+                    </div>
+                    @if (!$weekly_expense)
+                    <div class="alert alert-info">
+                        Ainda não foram gravadas as despesas desta semana
+                    </div>
+                    @endif
+                    @else
+                    <div class="alert alert-info">
+                        Deve selecionar uma empresa.
+                    </div>
+                    @endif
                 </div>
             </div>
 
@@ -21,3 +68,6 @@
     </div>
 </div>
 @endsection
+<script>
+    console.log({!! $company_expenses !!})
+</script>

8aafd0ea745b039608ed4f9f83f7e2a38afc78e7
marcosborg <m.borges.mail@gmail.com>
2023-11-08 12:03:29 +0000

    update

--- app/Http/Controllers/Admin/FinancialStatementController.php
+++ app/Http/Controllers/Admin/FinancialStatementController.php
@@ -257,9 +257,7 @@ class FinancialStatementController extends Controller
             }
         }
 
-        return $driver;
-
-        if ($driver->contract_vat->percent && $driver->contract_vat->percent > 0) {
+        if ($driver && $driver->contract_vat->percent && $driver->contract_vat->percent > 0) {
             $txt_admin = ($final_total * $driver->contract_vat->percent)/100;
             $gross_debts = $gross_debts + $txt_admin;
             $final_total = $final_total - $txt_admin;

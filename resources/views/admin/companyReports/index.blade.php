cdc4bfb84db704a73c6375bc7e4f26b613717be1
marcosborg <m.borges.mail@gmail.com>
2023-11-17 18:19:43 +0000

    Update

--- resources/views/admin/companyReports/index.blade.php
+++ resources/views/admin/companyReports/index.blade.php
@@ -73,7 +73,7 @@
                     @foreach ($drivers as $driver)
                     @if ($driver->earnings)
                     <tr>
-                        <td>{{ $driver->name }} {{ $driver->id }}</td>
+                        <td>{{ $driver->name }}</td>
                         <td style="text-align: right;">{{ number_format($driver->earnings['uber']['total_earnings'] ??
                             0, 2) }} <small>€</small></td>
                         <td style="text-align: right;">{{ number_format($driver->earnings['bolt']['total_earnings'] ??

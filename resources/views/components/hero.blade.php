8b7daeb189b9911bd2a97d5436dd7988e99a5ae0
marcosborg <m.borges.mail@gmail.com>
2024-03-12 08:35:09 +0000

    Update

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,10 +1,10 @@
 <!-- ======= Hero Section ======= -->
 <section id="hero" style="background-image: url('')">
     <div class="hero-container" data-aos="fade-up" data-aos-delay="150">
-        <h1>{{ $hero->title }}</h1>
-        <h2>{{ $hero->subtitle }}</h2>
+        <h1>Title</h1>
+        <h2>Subtitle</h2>
         <div class="d-flex">
-            <a href="{{ $hero->link }}" class="btn-get-started scrollto">{{ $hero->button }}</a>
+            <a href="Link" class="btn-get-started scrollto">Button</a>
         </div>
     </div>
 </section><!-- End Hero -->

e546bb63185a6f149298d8f087bc01e29f59b3ac
marcosborg <m.borges.mail@gmail.com>
2024-03-18 14:59:17 +0000

    Update

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,10 +1,15 @@
 <!-- ======= Hero Section ======= -->
 <section id="hero" style="background-image: url('')">
     <div class="hero-container" data-aos="fade-up" data-aos-delay="150">
-        <h1>Title</h1>
-        <h2>Subtitle</h2>
+        <h1>Login</h1>
+        <h2>backoffice</h2>
         <div class="d-flex">
-            <a href="Link" class="btn-get-started scrollto">Button</a>
+            @auth
+            <a href="/admin" class="btn-get-started scrollto">Dashboard</a>
+            @else
+            <a href="/login" class="btn-get-started scrollto">Login</a>
+            @endauth
+            
         </div>
     </div>
 </section><!-- End Hero -->

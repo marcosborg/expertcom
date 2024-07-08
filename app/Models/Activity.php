37c98d280cc4f53fb22b8dc541eeb3e77134f816
marcosborg <m.borges.mail@gmail.com>
2024-07-08 11:54:36 +0100

    Update

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -51,11 +51,15 @@ class Activity extends Model implements HasMedia
         return $date->format('Y-m-d H:i:s');
     }
 
+    /*
+
     public function registerMediaConversions(Media $media = null): void
     {
         $this->addMediaConversion('thumb')->fit('crop', 50, 50);
         $this->addMediaConversion('preview')->fit('crop', 120, 120);
     }
+
+    */
 
     public function getImageAttribute()
     {

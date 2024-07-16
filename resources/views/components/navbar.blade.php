c7801c2def2f2838edfbfb435413a4098030c80b
marcosborg <m.borges.mail@gmail.com>
2024-07-16 16:59:10 +0100

    Update

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,9 @@
 <nav id="navbar" class="navbar">
     <ul>
-        <li><a class="nav-link active" href="/">Home</a></li>
-        <li><a class="nav-link" href="#contact">Contact</a></li>
+        <li><a class="nav-link active" href="{{ request()->is('/') ? '#hero' : '/' }}">Início</a></li>
+        <li><a class="nav-link" href="{{ request()->is('/') ? '#about' : '/#about' }}">Quem somos</a></li>
+        <li><a class="nav-link" href="{{ request()->is('/') ? '#contact' : '/#contact' }}">Contactos</a></li>
+        <li><a class="nav-link" href="{{ request()->is('/') ? '#services' : '/#services' }}">Trabalhe connosco</a></li>
         @auth
         <li><a class="getstarted" href="/admin">Dashboard</a></li>
         @else

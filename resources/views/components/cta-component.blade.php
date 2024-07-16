e704000a6955cb6b77929ca4251110e59aea5822
marcosborg <m.borges.mail@gmail.com>
2024-07-16 11:07:45 +0100

    Update

--- resources/views/components/cta-component.blade.php
+++ resources/views/components/cta-component.blade.php
@@ -5,7 +5,7 @@
             <p>Fale com a Expertcom</p>
         </div>
         <div>
-            <p>Tem dúvidas sobre como pode trabalhar com a Expertcom sendo motorista ou uma empresa TVDE ? </p>
+            <p class="pb-4">Tem dúvidas sobre como pode trabalhar com a Expertcom sendo motorista ou uma empresa TVDE ? </p>
             <a href="https://linktr.ee/expertcom.pt" target="_new" class="btn-theme mt-2">Aceda já e esclarecemos as suas
                 dúvidas</a>
         </div>

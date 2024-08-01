fa14ba1d70d80cff7cfc50baabd9835163b18e8c
marcosborg <m.borges.mail@gmail.com>
2024-08-01 13:56:23 +0100

    Update

--- app/Http/Controllers/StandController.php
+++ app/Http/Controllers/StandController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\CarModel;
+use App\Models\Fuel;
+use App\Models\Origin;
 use Illuminate\Http\Request;
 use App\Models\StandCar;
 
@@ -10,7 +14,20 @@ class StandController extends Controller
 {
     public function index()
     {
-        $stand_cars = StandCar::all();
-        return $stand_cars;
+        $stand_cars = StandCar::all()->load([
+            'brand',
+            'car_model',
+            'fuel',
+            'month',
+            'origin',
+            'status'
+        ]);
+
+        $brands = Brand::all();
+        $models = CarModel::all();
+        $fuels = Fuel::all();
+        $origins = Origin::all();
+
+        return view('website.stand.index', compact('stand_cars', 'brands', 'models', 'fuels', 'origins'));
     }
 }

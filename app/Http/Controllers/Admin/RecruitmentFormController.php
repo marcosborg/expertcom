ea76b40168ac5c10c232dc5f7c1c89895e60a7d5
marcosborg <m.borges.mail@gmail.com>
2024-11-20 14:13:04 +0000

    Update

--- app/Http/Controllers/Admin/RecruitmentFormController.php
+++ app/Http/Controllers/Admin/RecruitmentFormController.php
@@ -101,7 +101,7 @@ class RecruitmentFormController extends Controller
         }
 
         if ($sendEmail == true) {
-            Notification::route('mail', 'marcosborges@netlook.pt'/*$recruitmentForm->company->email ?? 'info@expertcom.pt'*/)
+            Notification::route('mail', $recruitmentForm->company->email ?? 'info@expertcom.pt')
                 ->notify(new RecruitmentFormNotification($recruitmentForm, 'Alteração ao estado do recrutamento'));
         }
 
